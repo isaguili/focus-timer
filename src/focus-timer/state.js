fc72ec34789c2fd09808dd2075d466eabf350c3d
Isabella Silva <86665958+isaguili@users.noreply.github.com>
2024-04-10 07:31:52 -0300

    Add files via upload

---
+++ src/focus-timer/state.js
@@ -0,0 +1,12 @@
+export default {
+    minutes: 25,
+    seconds: 0,
+    
+    forestSound: false,
+    snowSound: false,
+    coffeeShopSound: false,
+    flameSound: false,
+    
+    isRunning: false,
+    countdownId: null
+}
